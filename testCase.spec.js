19ad6287bc80447150f08b7e7aa9b822229aae4c
devinbhullar71 <87924823+devinbhullar71@users.noreply.github.com>
2021-07-26 08:35:29 +1000

    Update testCase.spec.js

--- testCase.spec.js
+++ testCase.spec.js
@@ -13,15 +13,29 @@ describe('Given User is om Homepage When they search an item Then Add the Item i
     cy.visit('https://www.bunnings.com.au/')
   })
 
-  it('Enter search text into search box and click on search icon on site to add item to cart', () => {
+  it('Enter text into search box and click on search icon', () => {
     homePage.navigate()
     homePage.clickSearchTextInput();
     homePage.enterSearchText('hammer');
     homePage.clickSearchIcon();
-    homePage.verifySearchItemDisplayed()
+  });
+
+  it('Identify the first item displayed into the search results and add an item into the cart by clicking on to Add to cart icon button', () => {
+    homePage.navigate()
+    homePage.clickSearchTextInput();
+    homePage.enterSearchText('hammer');
+    homePage.clickSearchIcon();
+    homePage.verifySearchItemDisplayed('hammer')
+    homePage.addItemToCart();
+  });
+
+  it('Once an item added Review and checkout button is displayed on the screen', () => {
+    homePage.navigate()
+    homePage.clickSearchTextInput();
+    homePage.enterSearchText('hammer');
+    homePage.clickSearchIcon();
+    homePage.verifySearchItemDisplayed('hammer')
     homePage.addItemToCart();
     homePage.verifyReviewCheckoutButtonDisplayed();
-    homePage.clickReviewCheckOutButton();
-    homePage.verifyProductName();
   });
 });
